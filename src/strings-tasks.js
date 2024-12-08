cedcf91255a8e4b842fe6c50980072b71cb37661
Absolute-2004 <mixalobach@gmail.com>
2024-12-08 17:00:50 +0300

    feat: pass task removeLastOccurrences

--- src/strings-tasks.js
+++ src/strings-tasks.js
@@ -168,8 +168,11 @@ function removeFirstOccurrences(str, value) {
  *   removeLastOccurrences('I like legends', 'end') => 'I like legs'.
  *   removeLastOccurrences('ABABAB', 'BA') => 'ABAB'.
  */
-function removeLastOccurrences(/* str, value */) {
-  throw new Error('Not implemented');
+function removeLastOccurrences(str, value) {
+  const position = str.lastIndexOf(value);
+  return position === -1
+    ? str
+    : str.slice(0, position) + str.slice(position + value.length);
 }
 
 /**
